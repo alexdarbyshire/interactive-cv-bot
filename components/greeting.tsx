5e494d6cff0af7a48eab6d051ef6ba5989be58c9
alexdarbyshire <157761717+alexdarbyshire@users.noreply.github.com>
2025-06-21 18:17:01 +1000

    Add reference to blog post

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -24,7 +24,26 @@ export const Greeting = () => {
       >
         Ask me about Alex&apos;s experience, or let me create a tailored
         &apos;course of life&apos; (<em>curriculum vitae</em>), or we could do a{' '}
-        <em>résumé</em> if you prefer the French.
+        <em>résumé</em> should you prefer the French.
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 10 }}
+        transition={{ delay: 0.7 }}
+        className="mt-6 text-sm text-zinc-400"
+      >
+        <p>
+          Learn more about how this tool came about in this post:{' '}
+          <a
+            href="https://www.alexdarbyshire.com/2025/06/interactive-cv-bot-automating-a-less-than-fun-task/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:text-blue-400 underline transition-colors"
+          >
+            Interactive CV Bot: Automating a Less Than Fun Task
+          </a>
+        </p>
       </motion.div>
     </div>
   );
